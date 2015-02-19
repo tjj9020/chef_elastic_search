264e05019c3ff23e6a636a525eae77f945975fd7
Cloud User <tjj9020@gmail.com>
2015-02-19 18:23:56 +0000

    commit test

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'elastic_search'
 maintainer       'Tim Johnson'
 maintainer_email 'tjj9020@gmail.com'
 license          'All rights reserved'
-description      'Installs/Configures elasticsearch'
+description      'Installs/Configures ElasticSearch'
 long_description 'Installs/Configures elasticsearch'
 version          '0.1.11'
 
